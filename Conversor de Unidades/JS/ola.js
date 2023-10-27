9fbb96f664d91f0b7e8a3052dea4956c0be95205
SVJGarcia <126489521+SVJGarcia@users.noreply.github.com>
2023-10-27 12:44:40 -0300

    Add files via upload

--- Conversor de Unidades/JS/ola.js
+++ Conversor de Unidades/JS/ola.js
@@ -1,4 +1,17 @@
-//detecta o evento quando se escreve um número
-//chama a funçao
-visualViewport.addEventListener('keyuo', convertir);
-txtResultado = document.getElementById("resultado")
+/*
+//Converter  a temperetura de Fahrenheit para Celsius
+        function converteFparaC(F) {
+            var celsius = (F - 32) * (5/9)
+            return celsius;
+        }
+
+        //document.write(converteFparaC(212));
+*/
+        function converter(){
+            var distance_miles = document.getElementById("dados").value;
+             //1 milha é igual a 1,60934 quilômetros
+            let distance_km = distance_miles * 1.60934;
+            document.write.getElementById("resultado").innerHTML ='Quilômetros:'+ distance_km;
+            
+        }
+        
