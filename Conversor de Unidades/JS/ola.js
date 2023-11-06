54238cf334ca19e6fce6fab3802def59afe6e3ce
SVJGarcia <126489521+SVJGarcia@users.noreply.github.com>
2023-11-06 12:46:23 -0300

    Add files via upload

--- Conversor de Unidades/JS/ola.js
+++ Conversor de Unidades/JS/ola.js
@@ -1,17 +1,48 @@
-/*
+
+
 //Converter  a temperetura de Fahrenheit para Celsius
-        function converteFparaC(F) {
-            var celsius = (F - 32) * (5/9)
-            return celsius;
-        }
+     
 
         //document.write(converteFparaC(212));
-*/
+
         function converter(){
-            var distance_miles = document.getElementById("dados").value;
-             //1 milha é igual a 1,60934 quilômetros
+            var distance_miles = document.getElementById("milhas").value;
+            var tipo = document.getElementById("converte").value;
+           
+            //1 milha é igual a 1,60934 quilômetros
             let distance_km = distance_miles * 1.60934;
             document.write.getElementById("resultado").innerHTML ='Quilômetros:'+ distance_km;
-            
+            if (tipo==1){
+                let distance_km = distance_miles * 1.60934;
+                document.write.getElementById("resultado").innerHTML ='Quilômetros:'+ distance_km;
+            }else if(tipo==2){
+
+            }
+        }
+          /*          
+        //conversor de libras para km
+        "use strict";
+
+        Object.defineProperty(exports, "__esModule", {
+        value: true
+        });
+        exports.default = conversor;
+        function conversor(numero, unidad) {
+         var resultado = void 0;
+        if (unidad == "kg" || unidad == "kilos") {
+        return kiloALibra(numero);
+        } else if (unidad == "libra" || unidad == "lb") {
+        return libraAKilo(numero);
+        }
+        function kiloALibra(numero) {
+        let lb = numero * 2.20462;
+        resultado = lb.toFixed(2) + " lb";
+        return resultado;
+        }
+        function libraAKilo(numero) {
+        let kg = numero * 0.453592;
+        resultado = kg.toFixed(2) + " kg";
+        return resultado;
         }
         
+        }*/
