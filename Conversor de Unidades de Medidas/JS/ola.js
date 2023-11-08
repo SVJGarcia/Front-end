eb693c48f396fb4de3880085622e16d38153b573
SVJGarcia <126489521+SVJGarcia@users.noreply.github.com>
2023-11-08 17:01:28 -0300

    Add files via upload

---
+++ Conversor de Unidades de Medidas/JS/ola.js
@@ -0,0 +1,29 @@
+       //Uma milha equivale à 1604.38 metros.
+       var milhas = parseFloat(prompt("Digite a medida em Milhas:"));
+       var mi = 1604.38;
+       var metros = (milhas*mi) / (1000);
+       alert(milhas+" a medida em Quilômetros é: "+ metros);
+
+       //Um Pé equivale a 0.3048 Metros.
+       var pés = parseFloat(prompt("Digite o comprimento em Pés:"));
+       var ft = 0.3048;
+       var metros = (pés*ft);
+       alert(pés+" O comprimento em Metros é: "+ metros);
+
+       //Uma Libra equivale a 0.45359237 Quilôgramas.
+       var libras = parseFloat(prompt("Digite a massa em Libras:"));
+       var lb = 0.45359237;
+       var quilos = (libras*lb);
+       alert(libras+" A massa em Quilôgramas é: "+ quilos);
+
+       //Um Galão equivale a 3.785411784 Litros.
+       var galões = parseFloat(prompt("Digite o volume em Galões:"));
+       var gal = 3.785411784;
+       var litros = (galões*gal);
+       alert(galões+" O volume em Litros é: "+ litros);
+
+       //Um Dólar equivale a 4.91 Reais.
+       var dólar = parseFloat(prompt("Digite o valor em Dólares:"));
+       var us = 4.91;
+       var reais = (dólar*us);
+       alert(dólar+" O valor em Reais é: "+ reais);
