338bd36f0572385527a325be25b867d0c8de25ac
SVJGarcia <126489521+SVJGarcia@users.noreply.github.com>
2023-11-10 12:48:02 -0300

    Add files via upload

--- Conversor de Unidades de Medidas/JS/ola.js
+++ Conversor de Unidades de Medidas/JS/ola.js
@@ -1,4 +1,59 @@
-       //Uma milha equivale à 1604.38 metros.
+function buscadado(){
+       var escolha=document.getElementById("converte").value;
+       if(escolha==1){
+              document.getElementById("descricao").textContent = ''
+              document.getElementById("descricao").textContent = 'Digite o valor em milhas:'
+             
+       }else if(escolha==2){
+              document.getElementById("descricao").textContent = ''
+              document.getElementById("descricao").textContent = 'Digite o valor em Pés:'
+             
+       }else if(escolha==3){
+              document.getElementById("descricao").textContent = ''
+              document.getElementById("descricao").textContent = 'Digite o valor em Libras:'
+              
+       }else if(escolha==4){
+              document.getElementById("descricao").textContent = ''
+              document.getElementById("descricao").textContent = 'Digite o valor em Galões:'
+              
+       }else if(escolha==5){
+              document.getElementById("descricao").textContent = ''
+              document.getElementById("descricao").textContent = 'Digite o valor em Dólares:'
+            
+       }else{
+              alert('garcia');
+       }
+}
+function blbl(){
+       var escolha=document.getElementById("converte").value;
+       var Ventrada = document.getElementById("valor").value;
+       if(escolha==1){
+              var mi = 1604.38;
+              var metros = (Ventrada*mi) / (1000);
+              document.getElementById("convertido").textContent =Ventrada+' a medida em Quilômetros é: '+metros;
+       }else if(escolha==2){
+              var ft = 0.3048;
+              var metros = (Ventrada*ft);
+              document.getElementById("convertido").textContent = 'pés '+Ventrada+'Valor em metros'+metros;
+       }else if(escolha==3){
+              var lb = 0.45359237;
+              var quilos = (Ventrada*lb);
+              document.getElementById("convertido").textContent = 'libras'+Ventrada+'Valor em quilos'+quilos;
+       }else if(escolha==4){
+              var gal = 3.785411784;
+              var litros = (Ventrada*gal);
+              document.getElementById("convertido").textContent = 'galoes'+Ventrada+'Valor em galões'+litros;
+       }else if(escolha==5){
+              var us = 4.92;
+              var reais = (Ventrada*us);
+              document.getElementById("descricao").textContent = 'dólares'+Ventrada+'Valor em Dólares'+reais
+       }else{
+              alert('garcia');
+       }
+}
+
+/*
+      //Uma milha equivale à 1604.38 metros.
        var milhas = parseFloat(prompt("Digite a medida em Milhas:"));
        var mi = 1604.38;
        var metros = (milhas*mi) / (1000);
@@ -23,7 +78,8 @@
        alert(galões+" O volume em Litros é: "+ litros);
 
        //Um Dólar equivale a 4.91 Reais.
-       var dólar = parseFloat(prompt("Digite o valor em Dólares:"));
-       var us = 4.91;
+       var dólar = parseFloat(prompt("Digite o descricao em Dólares:"));
+       var us = 4.92;
        var reais = (dólar*us);
-       alert(dólar+" O valor em Reais é: "+ reais);
+       alert(dólar+" O descricao em Reais é: "+ reais);
+*/
